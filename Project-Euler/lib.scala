79ade0ea35f147aeefb61e0214fc9466052e4e1c
Davi Barbosa <dbarbosa@gmail.com>
2012-12-07 04:39:24 -0200

    Project Euler 64

--- Project-Euler/lib.scala
+++ Project-Euler/lib.scala
@@ -1,6 +1,8 @@
 package PELib
 
 object PELib {
+  def ints(i: Int) : Stream[Int] = i #:: ints(i+1)
+
   def getDigits(n : BigInt) : List[Int] = {
     def f(n : BigInt, r : List[Int]) : List[Int] =
       if (n == 0) r
@@ -10,4 +12,14 @@ object PELib {
   
   def digitsToNumber(digits : List[Int]) : BigInt =
     digits.foldLeft(BigInt(0))(10 * _ + _)
+
+  def gcd(a : Int, b : Int) : Int =
+    if (b == 0) a
+    else gcd(b, a % b)
+
+  def perfectSquare(n : Int) : Boolean = {
+    val sqrt = math.sqrt(n).toInt
+    if (sqrt * sqrt == n) true
+    else false
+  }
 }
---
+++ Project-Euler/problem64.scala
@@ -0,0 +1,45 @@
+import PELib._
+
+object ProjectEuler {
+  def gcd3(a : Int, b : Int, c : Int) =
+    PELib.gcd(PELib.gcd(math.abs(a), math.abs(b)), math.abs(c))
+
+  def representation(number : Int) : List[Int] = {
+    val sqrt = math.sqrt(number)
+    def invert(A : Int, B : Int, C : Int) : (Int, Int, Int) = {
+      // (A*sqrt(number) + B) / C  --> C / (A*sqrt(number) + B)
+      val denominator = A*A*number - B*B
+      val div : Int = gcd3(C*A, C*B, denominator)
+      (C*A/div, -C*B/div, denominator/div)
+    }
+    def f(A : Int, B : Int, C : Int, seen : Set[(Int, Int, Int)]) : List[Int] = {
+      // (A*sqrt(number) + B) / C
+      if (seen contains (A, B, C)) Nil
+      else {
+        val floor : Int = math.floor((A * sqrt + B) / C).toInt
+//        System.out.println("( " + A + "*sqrt(" + number + ") + " + B + " )/" + C
+//                           + "   --> " + floor)
+        val (a, b, c) = invert(A, B - floor*C, C)
+        floor :: f(a, b, c, seen + ((A,B,C)))
+      }
+    }
+    val floor : Int = math.floor(sqrt).toInt
+    if (floor * floor == number) List(floor)
+    else {
+      floor :: f(1, floor, number - floor*floor, Set())
+    }
+  }
+
+  def cycleLength(continued_fraction : Stream[Int]) : Int =
+    continued_fraction match {
+      case _ #:: first #:: t => t.indexOf(first) + 1
+      case _ => 0
+    }
+
+  def main(args: Array[String]) {
+    val n = 10000
+    val numbers = PELib.ints(1).filterNot(PELib.perfectSquare).takeWhile(_ <= n)
+    val how_many = numbers.count(i => (representation(i).length - 1) % 2 == 1)
+    System.out.println(how_many)
+  }
+}
